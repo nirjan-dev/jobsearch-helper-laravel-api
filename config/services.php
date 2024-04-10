10a67c824f0c1291cdcf17e6d06d82d2cef19240
nirjankhadka <nirjan.khadka@gfinity.net>
2024-04-10 11:18:48 +0545

    fix: prod issues with oauth and email

--- config/services.php
+++ config/services.php
@@ -34,19 +34,13 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => '/auth/callback/github',
+        'redirect' => env('APP_URL').'/auth/callback/github',
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => '/auth/callback/google',
-    ],
-
-    'linkedin' => [
-        'client_id' => env('LINKEDIN_CLIENT_ID'),
-        'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
-        'redirect' => '/auth/callback/linkedin',
+        'redirect' => env('APP_URL').'/auth/callback/google',
     ],
 
 ];
